3d287ea2f6dc626f1e74d473eba9d1ec3c9ef399
Shanmuka_RS25 <164605613+ShanmukaRS25@users.noreply.github.com>
2024-08-08 13:16:35 +0530

    Delete tracker directory

--- tracker/urls.py
+++
@@ -1,9 +0,0 @@
-
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('expenses/', views.expense_list, name='expense_list'),
-    path('add_expense/', views.add_expense, name='add_expense'),
-]
